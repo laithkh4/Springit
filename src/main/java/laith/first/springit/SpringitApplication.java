d84bb63013fd31b24cbf9604b27892af883b188e
laithkh4 <laith.1999>
2021-11-21 15:26:07 +0200

    adding welcome message

--- src/main/java/laith/first/springit/SpringitApplication.java
+++ src/main/java/laith/first/springit/SpringitApplication.java
@@ -1,14 +1,19 @@
 package laith.first.springit;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 
 @SpringBootApplication
+
 public class SpringitApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringitApplication.class, args);
-        System.out.println("welecome to springit !");
+
     }
 
 }
