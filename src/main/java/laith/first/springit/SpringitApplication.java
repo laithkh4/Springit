d1dcb25f1f2eaf672525892f0bfd449a53d24062
laithkh4 <laith.1999>
2021-11-22 14:46:47 +0200

    Create Entities and refactor using project lombok

--- src/main/java/laith/first/springit/SpringitApplication.java
+++ src/main/java/laith/first/springit/SpringitApplication.java
@@ -1,19 +1,37 @@
 package laith.first.springit;
 
-
-
+/*
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+*/
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
+/*
+import org.slf4j.Logger;
+import org.springframework.context.annotation.Bean;
+*/
 
 @SpringBootApplication
 
 public class SpringitApplication {
+ /*   private static final Logger log= LoggerFactory.getLogger(SpringitApplication.class);
+   */
+ public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringitApplication.class, args);
+    SpringApplication.run(SpringitApplication.class, args);
 
     }
+ /*   @Bean
+    CommandLineRunner runner(){
+        return args -> {
+            log.error("CommandLineRunner.run()");
+            log.warn("CommandLineRunner.run()");
+            log.info("CommandLineRunner.run()");
+            log.debug("CommandLineRunner.run()");
+            log.trace("CommandLineRunner.run()");
 
+        };
+    }
+
+*/
 }
---
+++ src/main/java/laith/first/springit/domain/Vote.java
@@ -0,0 +1,21 @@
+package laith.first.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Vote {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private int vote;
+
+
+
+}
